784035def910fbbe076fddaecec9d93521dc1b36
dmitriysemenko <dm123semenko@gmail.com>
2021-07-09 19:56:44 +0100

    update

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,10 +3,13 @@ import React from "react";
 class StorePicker extends React.Component {
     render() {
         return (
-            <React.Fragment>
-               <h1>Name</h1>
-               <p>Description</p>
-            </React.Fragment>
+            <>
+                <form className="store-selector">
+                    <h2>Please Enter A Store</h2>
+                    <input type="text" required placeholder="Store Name"/>
+                    <button type="submit">Visit Store →</button>
+                </form>
+            </>
         );
     }
 }
