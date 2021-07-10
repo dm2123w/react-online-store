ef47c7dbcb155e97b1305db59d4a4ad2a08e5db3
dmitriysemenko <dm123semenko@gmail.com>
2021-07-10 22:03:17 +0100

    update

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
     render() {
@@ -6,7 +7,12 @@ class StorePicker extends React.Component {
             <>
                 <form className="store-selector">
                     <h2>Please Enter A Store</h2>
-                    <input type="text" required placeholder="Store Name"/>
+                    <input
+                        type="text"
+                        required
+                        placeholder="Store Name"
+                        defaultValue={getFunName}
+                    />
                     <button type="submit">Visit Store →</button>
                 </form>
             </>
