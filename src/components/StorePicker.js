2ba454fa3c82ca8c81a109158fb0953f15cc7726
dmitriysemenko <dm123semenko@gmail.com>
2021-07-11 22:46:00 +0100

    update

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -8,25 +8,24 @@ class StorePicker extends React.Component {
         // 1. Stop the form from submitting
         event.preventDefault();
         // 2. Get the text from that input
-        console.log(this);
+        const storeName = this.myInput.value.value;
         // 3. Change the page to /store/whatever-they-entered
-    }
+        this.props.history.push(`/store/${storeName}`);
+    };
 
     render() {
         return (
-            <>
-                <form className="store-selector" onSubmit={this.goToStore}>
-                    <h2>Please Enter A Store</h2>
-                    <input
-                        type="text"
-                        ref={this.myInput}
-                        required
-                        placeholder="Store Name"
-                        defaultValue={getFunName()}
-                    />
-                    <button type="submit">Visit Store →</button>
-                </form>
-            </>
+            <form className="store-selector" onSubmit={this.goToStore}>
+                <h2>Please Enter A Store</h2>
+                <input
+                    type="text"
+                    ref={this.myInput}
+                    required
+                    placeholder="Store Name"
+                    defaultValue={getFunName()}
+                />
+                <button type="submit">Visit Store →</button>
+            </form>
         );
     }
 }
