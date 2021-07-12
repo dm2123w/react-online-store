a8dbf96205f1c15ff89332cae7bef5f5bb7f0516
dmitriysemenko <dm123semenko@gmail.com>
2021-07-12 20:52:25 +0300

    update

--- src/components/AddFishForm.js
+++ src/components/AddFishForm.js
@@ -2,9 +2,19 @@ import React from "react";
 
 class AddFishForm extends React.Component {
     render() {
-        return <form>
-            <p>It works</p>
-        </form>;
+        return (
+            <form claaName="fish-edit">
+                <input name="name" type="text" placeholder="Name" />
+                <input name="price" type="text" placeholder="Price" />
+                <select name="status" type="text">
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold Out!</option>
+                </select>
+                <textarea name="desc" placeholder="Desc" />
+                <input name="image" type="text" placeholder="Image" />
+                <button type="submit">+ Add Fish</button>
+            </form>
+        );
     }
 }
 
