b6488cc80d27dc0da1f62c06056f77d4f5619fb4
dmitriysemenko <dm123semenko@gmail.com>
2021-07-12 20:28:25 +0300

    update

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+    render() {
+        return <form>
+            <p>It works</p>
+        </form>;
+    }
+}
+
+export default AddFishForm;
