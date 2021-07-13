c2e8c240c2edcca1fd4320d2368f7040f816838f
dmitriysemenko <dm123semenko@gmail.com>
2021-07-13 22:57:09 +0300

    update

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,6 +2,26 @@ import React from "react";
 import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
+    renderOrder = (key) => {
+        const fish = this.props.fishes[key];
+        const count = this.props.order[key];
+        const isAvailable = fish.status === "available";
+        if (!isAvailable) {
+            return (
+                <li key={key}>
+                    Sorry {fish ? fish.name : "fish"} is no longer available
+                </li>
+            );
+        }
+
+        return (
+            <li key={key}>
+                {count} lbc {fish.name}
+                {formatPrice(count * fish.price)}
+            </li>
+        );
+    };
+
     render() {
         const orderIds = Object.keys(this.props.order);
         const total = orderIds.reduce((prevTotal, key) => {
@@ -17,7 +37,7 @@ class Order extends React.Component {
         return (
             <div className="order-wrap">
                 <h2>Order</h2>
-                {orderIds}
+                <ul className="order">{orderIds.map(this.renderOrder)}</ul>
                 <div className="total">
                     <strong>{formatPrice(total)}</strong>
                 </div>
