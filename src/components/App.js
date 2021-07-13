1e70c67eb82f7b5571af9d278f0fe8b797fdab78
dmitriysemenko <dm123semenko@gmail.com>
2021-07-13 20:03:39 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
-
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 class App extends React.Component {
     state = {
         fishes: {},
@@ -19,14 +20,23 @@ class App extends React.Component {
             // fishes: fishes,
         });
     };
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    };
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Catch of the day" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                    </ul>
                 </div>
                 <Order />
-                <Inventory addFish={this.addFish} />
+                <Inventory
+                    addFish={this.addFish}
+                    loadSampleFishes={this.loadSampleFishes}
+                />
             </div>
         );
     }
