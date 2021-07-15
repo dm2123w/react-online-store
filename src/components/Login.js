fad1486be8aa0bc330307dae0b6ee50b041b2338
dmitriysemenko <dm123semenko@gmail.com>
2021-07-15 14:29:32 +0300

    update

---
+++ src/components/Login.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+const Login = (props) => (
+    <nav className="login">
+        <h2>Intentory Login</h2>
+        <p>Sign in to manage your store's inventory.</p>
+        <button
+            className="github"
+            onClick={() => props.authenticate("Github")}
+        >
+            Log In With Github
+        </button>
+        <button
+            className="facebook"
+            onClick={() => props.authenticate("Facebook")}
+        >
+            Log In With Facebook
+        </button>
+    </nav>
+);
+
+Login.propTypes = {
+    authenticate: PropTypes.func.isRequired
+};
+
+export default Login;
